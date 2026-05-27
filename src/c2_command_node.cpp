6f8989a5b90dfa54c896d0f50357a21efd232fec
root <root@prime.localdomain>
2026-05-27 16:19:14 +0900

    nats 링크 오류 수정

--- src/c2_command_node.cpp
+++ src/c2_command_node.cpp
@@ -8,6 +8,8 @@
 #include <px4_msgs/msg/offboard_control_mode.hpp>
 //#include <cuas_msgs/msg/c2_command.hpp>
 
+#include <nats/nats.h>
+
 class C2CommandNode : public rclcpp::Node
 {
 public:
@@ -18,15 +20,57 @@ public:
     publisher_ = this->create_publisher<std_msgs::msg::String>(cuas_datalink::topics::C2_COMMAND,cuas_datalink::ReliableControlQoS());
     timer_ = this->create_wall_timer(std::chrono::milliseconds(publish_period_ms_),std::bind(&C2CommandNode::PublishCommand, this));
     RCLCPP_INFO(this->get_logger(), "c2_command_node started");
+
+    ConnectNats();
   }
 
 private:
+
+  void ConnectNats()
+  {
+
+    nats_url_ = this->declare_parameter<std::string>("nats_url", "nats://127.0.0.1:4222");
+
+    natsOptions * opts = nullptr;
+    natsOptions_Create(&opts);
+    natsOptions_SetURL(opts, nats_url_.c_str());
+    natsOptions_SetReconnectWait(opts, 500);
+    natsOptions_SetMaxReconnect(opts, -1);
+
+    natsStatus status = natsConnection_Connect(&conn_, opts);
+    natsOptions_Destroy(opts);
+
+    if (status != NATS_OK) {
+      const char * err = natsStatus_GetText(status);
+      RCLCPP_FATAL(this->get_logger(), "NATS connect failed: %s", err);
+      throw std::runtime_error(err);
+    }
+
+    running_ = true;
+  }
+
+
+
   void PublishCommand()
   {
     std_msgs::msg::String msg;
     msg.data = command_prefix_ + " seq=" + std::to_string(sequence_++);
     publisher_->publish(msg);
   }
+
+
+  std::string nats_url_;
+  std::string subject_c2_command_;
+  std::string subject_c2_status_;
+  std::string subject_interceptor_command_;
+  std::string subject_interceptor_status_;
+  std::string subject_heartbeat_;
+  natsConnection * conn_ = nullptr;
+  std::vector<natsSubscription *> subscriptions_;
+  //std::vector<std::unique_ptr<NatsCallbackContext>> callback_contexts_;
+  std::mutex nats_mutex_;
+  std::atomic_bool running_;
+
 
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
